4bf1fa87d1e503181b821aba27603437cf5dfce9
narcpat <narcissearts@gmail.com>
2022-03-12 15:10:02 -0500

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
-// Assignment code here
+// Alert with password Criteria
 
+// Prompt for password length
+
+// Prompt for characters to be included
+// use upper characters?
+
+// use lower characters?
+
+// use numbers?
+
+// use special chararacters?
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +20,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
